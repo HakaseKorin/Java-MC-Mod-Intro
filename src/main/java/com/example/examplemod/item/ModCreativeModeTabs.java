e6a262e70409386100ca7dcfb426557b65071595
Arby Gudes <aegudes@gmail.com>
2024-04-09 13:48:14 -0500

    adds strawberry foods

--- src/main/java/com/example/examplemod/item/ModCreativeModeTabs.java
+++ src/main/java/com/example/examplemod/item/ModCreativeModeTabs.java
@@ -26,6 +26,8 @@ public class ModCreativeModeTabs {
 
                         pOutput.accept(ModItems.METAL_DETECTOR.get());
 
+                        pOutput.accept(ModItems.STRAWBERRY.get());
+
                         pOutput.accept(ModBlocks.SAPPHIRE_ORE.get());
                         pOutput.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
                         pOutput.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
