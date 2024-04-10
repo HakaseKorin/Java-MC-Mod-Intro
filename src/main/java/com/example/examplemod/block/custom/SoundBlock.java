f637e92bab780db1122a7dbdacdc751018677865
Arby Gudes <aegudes@gmail.com>
2024-04-10 13:57:54 -0500

    adds custom tooltips to some modded items.
    note: for durability, can only be tested in survival.
    added note for differences between literal and translatable.

--- src/main/java/com/example/examplemod/block/custom/SoundBlock.java
+++ src/main/java/com/example/examplemod/block/custom/SoundBlock.java
@@ -1,15 +1,22 @@
 package com.example.examplemod.block.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class SoundBlock extends Block {
     public SoundBlock(Properties pProperties) {
@@ -24,5 +31,13 @@ public class SoundBlock extends Block {
                 1f,1f);
 
         return InteractionResult.SUCCESS;
+    }
+
+    // literal vs translatable - translatable can be translated to other languages in the respective language json file
+    // as literal is a static string that is read in directly from the code and will only be in the language coded in.
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable BlockGetter pLevel, List<Component> pTooltip, TooltipFlag pFlag) {
+        pTooltip.add(Component.literal("Makes sweet sounds when right-clicked"));
+        super.appendHoverText(pStack, pLevel, pTooltip, pFlag);
     }
 }
